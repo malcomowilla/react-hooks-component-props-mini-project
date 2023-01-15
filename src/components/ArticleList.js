fa023b3ba1c2ba241fa7fbdf9838d1060a4e6e2d
malcomowilla <owillamalcom@gmail.com>
2023-01-15 14:06:46 +0300

    completed

---
+++ src/components/ArticleList.js
@@ -0,0 +1,15 @@
+import Article from "./Article";
+
+function ArticleList(){
+    console.log(props)
+    return(
+        <main>
+            {props.posts.map((post)=>(
+               <Article title={post.title} date={post.date} preview={post.preview} minutes={post.minutes} key={post.id} />
+            ))}
+
+        </main>
+    )
+}
+
+export default ArticleList
